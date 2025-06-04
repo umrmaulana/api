7e8d90d74e850c6a6d6fccb2981177bc51d7bf9e
umrmaulana <maulanau147@gmail.com>
2025-06-04 22:30:28 +0700

    update post_ship_address.php to include province_name, city_name, and postal_code in the request parameters and database insertion

--- post_ship_address.php
+++ post_ship_address.php
@@ -8,15 +8,21 @@ $response = array();
 if (
     isset($_POST['user_id']) &&
     isset($_POST['province_id']) &&
+    isset($_POST['province_name']) &&
     isset($_POST['city_id']) &&
+    isset($_POST['city_name']) &&
     isset($_POST['address']) &&
-    isset($_POST['recipt_name'])
+    isset($_POST['recipt_name']) &&
+    isset($_POST['postal_code'])
 ) {
     $user_id = $_POST['user_id'];
     $province_id = $_POST['province_id'];
+    $province_name = $_POST['province_name'];
     $city_id = $_POST['city_id'];
+    $city_name = $_POST['city_name'];
     $address = $_POST['address'];
     $recipt_name = $_POST['recipt_name'];
+    $postal_code = $_POST['postal_code'];
 
     // Validasi sederhana
     if (empty($address) || empty($recipt_name)) {
@@ -28,10 +34,20 @@ if (
         exit();
     }
 
-    // Query insert
-    $stmt = $conn->prepare("INSERT INTO ship_addresses (user_id, province_id, city_id, address, recipt_name) 
-                            VALUES (?, ?, ?, ?, ?)");
-    if ($stmt->execute([$user_id, $province_id, $city_id, $address, $recipt_name])) {
+    $stmt = $conn->prepare("INSERT INTO ship_addresses (user_id, province_id, province_name,city_id, city_name, address, recipt_name, postal_code) 
+                            VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    if (
+        $stmt->execute([
+            $user_id,
+            $province_id,
+            $province_name,
+            $city_id,
+            $city_name,
+            $address,
+            $recipt_name,
+            $postal_code
+        ])
+    ) {
         http_response_code(200);
         echo json_encode([
             "success" => true,
